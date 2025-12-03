7f69f8630932de3036cf834890e4d9c5ae49ae7c
morauszkia <morauszki.andras@gmail.com>
2025-12-03 19:33:07 +0100

    Chore: Prepare deployment to GitHub Pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -5,7 +5,7 @@ from md_to_html import markdown_to_html_node
 from extract_title import extract_title
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     if not os.path.exists(from_path):
@@ -32,18 +32,20 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_content)
+    template = template.replace("href=\"/" , f"href=\"{basepath}")
+    template = template.replace("src=\"/", f"src=\"{basepath}")
 
     with open(dest_path, "w") as file:
         file.write(template)
     
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for entry in os.listdir(dir_path_content):
         full_path = os.path.join(dir_path_content, entry)
         dest_path = os.path.join(dest_dir_path, entry)
         if os.path.isdir(full_path):
-            generate_pages_recursive(full_path, template_path, dest_path)
+            generate_pages_recursive(full_path, template_path, dest_path, basepath)
         elif entry.endswith(".md"):
             dest_path = re.sub(r"md$", r"html", dest_path)
-            generate_page(full_path, template_path, dest_path)
+            generate_page(full_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
+import sys
 from copystatic import copy_static_files, empty_directory
 from generate_page import generate_pages_recursive
 
 
+DST_DIR = "docs"
+
 def main():
-    empty_directory("public")
-    copy_static_files("static", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    empty_directory(DST_DIR)
+    copy_static_files("static", DST_DIR)
     print("Finished copying files.")
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", DST_DIR, basepath)
     print("All pages generated.")
 
 
